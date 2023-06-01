ae52ed9eb133806ddeb0702ad0c3ca37c76484e9
Karen Almeida <karenalmeida61@hotmail.com>
2023-06-01 03:33:17 -0300

    first commit

---
+++ src/main/java/com/serasa/experian/HotelExperian/HotelExperianApplication.java
@@ -0,0 +1,13 @@
+package com.serasa.experian.HotelExperian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelExperianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelExperianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serasa/experian/HotelExperian/HotelExperianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serasa.experian.HotelExperian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelExperianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
