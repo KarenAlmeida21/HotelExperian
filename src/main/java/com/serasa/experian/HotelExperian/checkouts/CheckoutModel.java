ed6ad0dafe5e906983d0387a772c31833c311ebf
Karen Almeida <karenalmeida61@hotmail.com>
2023-06-01 08:35:20 -0300

    Camada model/service/controller de checkout

---
+++ src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutModel.java
@@ -0,0 +1,4 @@
+package com.serasa.experian.HotelExperian.checkouts;
+
+public class CheckoutModel {
+}
---
+++ src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutRepository.java
@@ -0,0 +1,6 @@
+package com.serasa.experian.HotelExperian.checkouts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<> {
+}
