2aeeedf0a3b727cb02d3de031243267956883bf5
Karen Almeida <karenalmeida61@hotmail.com>
2023-06-01 08:42:03 -0300

    Entidade checkout

--- src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutModel.java
+++ src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutModel.java
@@ -1,4 +1,28 @@
 package com.serasa.experian.HotelExperian.checkouts;
 
+import com.serasa.experian.HotelExperian.CheckIns.CheckInModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class CheckoutModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "check_in_id")
+    private CheckInModel checkIn;
+    private LocalDate dataDaSaSaida = LocalDate.now();
+    private Double valorDaHospedagem;
+
 }
--- src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutRepository.java
+++ src/main/java/com/serasa/experian/HotelExperian/checkouts/CheckoutRepository.java
@@ -2,5 +2,5 @@ package com.serasa.experian.HotelExperian.checkouts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CheckoutRepository extends JpaRepository<> {
+public interface CheckoutRepository extends JpaRepository<CheckoutModel, Long> {
 }
