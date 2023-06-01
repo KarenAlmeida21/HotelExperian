3fd1dfb821865f12c0c9ce0aec2175e382a56995
Karen Almeida <karenalmeida61@hotmail.com>
2023-06-01 04:18:06 -0300

    Inserir metodo para cadastro Hospede com verificacoes

--- src/main/java/com/serasa/experian/HotelExperian/Hospedes/HospedeRepository.java
+++ src/main/java/com/serasa/experian/HotelExperian/Hospedes/HospedeRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HospedeRepository extends JpaRepository<HospedeModel, String> {
+    boolean existsByNomeHospede(String nome);
+
+    boolean existsByDocumento(String documento);
 }
--- src/main/java/com/serasa/experian/HotelExperian/Hospedes/HospedeService.java
+++ src/main/java/com/serasa/experian/HotelExperian/Hospedes/HospedeService.java
@@ -11,7 +11,25 @@ public class HospedeService {
         this.hospedeRepository = hospedeRepository;
     }
 
+    public HospedeModel cadastraHospede(HospedeModel hospede) {
+        verificaHospedePorNome(hospede.getNomeHospede());
+        verificaDocumentoHospede(hospede.getDocumento());
+
+        return hospedeRepository.save(hospede);
+    }
+
+    public void verificaHospedePorNome(String nome) {
+        if (hospedeRepository.existsByNomeHospede(nome)) {
+            throw new HospedeException("Hospede com o mesmo nome já cadastrado.");
+        }
+    }
+
+    public void verificaDocumentoHospede(String documento) {
+        if (hospedeRepository.existsByDocumento(documento)) {
+            throw new HospedeException("Hóspede com o mesmo documento já cadastrado.");
+        }
+    }
 
 
-    
+
 }
