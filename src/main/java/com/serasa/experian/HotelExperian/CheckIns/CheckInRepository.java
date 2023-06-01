d7faec28f90ffb003c9703523b49dd9a8c1b2f92
Karen Almeida <karenalmeida61@hotmail.com>
2023-06-01 05:52:18 -0300

    adicionar DTO para checkin

--- src/main/java/com/serasa/experian/HotelExperian/CheckIns/CheckInRepository.java
+++ src/main/java/com/serasa/experian/HotelExperian/CheckIns/CheckInRepository.java
@@ -1,2 +1,8 @@
-package com.serasa.experian.HotelExperian.CheckIns;public interface CheckInRepository {
+package com.serasa.experian.HotelExperian.CheckIns;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckInRepository extends JpaRepository {
 }
